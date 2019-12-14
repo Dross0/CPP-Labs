751c5116f3bb635821730c453c8bffb5e83d7e3e
Dross0 <andrey04072000@gmail.com>
2019-12-14 21:27:15 +0700

    Remove  cmake-debug

--- task1/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/lib/googletest/googlemock
-# Build directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- task1/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/lib/googletest/googletest
-# Build directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
