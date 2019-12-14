252977388e1f4c0a45e69157b2edc55466920dfa
Dross0 <andrey04072000@gmail.com>
2019-12-14 20:52:41 +0700

    Add Google Test Library

---
+++ task1/cmake-build-debug/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/lib/googletest
+# Build directory: /Users/andreygaidamaka/Public/Учеба/OOOP/С++/Лабы/OOP/task1/cmake-build-debug/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
